38279d1f379a0f0ad573305005a1eb50293deafe
cristianblanco <blancochoquecristian123@gmail.com>
2023-03-14 20:46:05 -0300

    Se agrega capas corregidas

--- src/run.js
+++ src/run.js
@@ -2,8 +2,6 @@ import productRouter from "./routers/products.router.js"
 import cartRouter from "./routers/cart_router.js"
 import chatRouter from "./routers/chat_router.js"
 import messagesModel from "./dao/models/messages_model.js";
-import homeproduct from './routers/home_product.js'
-import realtime from './routers/real_time_products.js'
 import productViewsRouter from './routers/products_views_router.js'
 import sessionRouter from './routers/session_router.js'
 import { passportCall } from "./utils.js";
@@ -14,16 +12,7 @@ const run = (socketServer, app) => {
         req.io = socketServer
         next()
     })
-// // Middleware
-// function requireAuth(req, res, next) {
-//     if(req.session?.user) {
-//         return next()
-//     } else {
-//         return res.status(401).json({status: 'error', payload: 'not authenticated'})
-//     }
-// }
-    app.use('/',homeproduct)
-    app.use('/',realtime)
+
     app.use("/session", sessionRouter)
     app.use("/products",passportCall("jwt"), productViewsRouter)
     app.use("/api/products", productRouter)
